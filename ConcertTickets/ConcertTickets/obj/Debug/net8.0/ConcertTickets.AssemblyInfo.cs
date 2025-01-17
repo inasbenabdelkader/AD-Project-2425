a687965912377b842516cf3c3ebb2651dcff44c5
ns <inas.benabdelkader@student.odisee.be>
2025-01-17 23:22:12 +0100

    ConcertTickets
    Done   :)

--- ConcertTickets/ConcertTickets/obj/Debug/net8.0/ConcertTickets.AssemblyInfo.cs
+++ ConcertTickets/ConcertTickets/obj/Debug/net8.0/ConcertTickets.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConcertTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1b070c31b5ae5f3357e46cbd8e465c92b708af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1717738e61c94d5d56219d0f8829d8bcb748ba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConcertTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConcertTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
